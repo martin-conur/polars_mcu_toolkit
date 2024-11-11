ec66aa45e5fb8b7e4a88569af28ffc44f6871555
Martin Contreras <martin.contreras.u@outlook.com>
2024-11-11 17:27:35 -0300

    fixing tests

--- tests/test_negsample.py
+++ tests/test_negsample.py
@@ -4,7 +4,7 @@ from polars_mcu_toolkit import neg_sample
 
 def test_negsample():
 
-    superset = [1, 2, 3, 4]
+    superset = [0, 1, 2, 3]
     df = pl.DataFrame({'dense': [[0, 1], [2, 3]]})
     neg_sampled_df = df.with_columns(
         neg_sample("dense", superset).list.sort()
@@ -15,5 +15,5 @@ def test_negsample():
             "dense": [[2, 3], [0, 1]]
         }
     )
-
+    
     assert neg_sampled_df.equals(expected_df)
